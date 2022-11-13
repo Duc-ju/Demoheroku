dd36e2e48bcb664809c701fb0a559e4113682081
huyenxam <huyenxam250400@gmail.com>
2022-11-13 16:49:45 +0700

    first commit

--- app.py
+++ app.py
@@ -1,22 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template, request
 from transformers import pipeline
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def home():
-    model_checkpoint = "nguyenvulebinh/vi-mrc-large"
-    # model_checkpoint = "nguyenvulebinh/vi-mrc-base"
-    nlp = pipeline('question-answering', model=model_checkpoint,
-                      tokenizer=model_checkpoint)
-    # QA_input = {
-    #   'question': "Bình là chuyên gia về gì ?",
-    #   'context': "Bình Nguyễn là một người đam mê với lĩnh vực xử lý ngôn ngữ tự nhiên . Anh nhận chứng chỉ Google Developer Expert năm 2020"
-    # }
-    # res = nlp(QA_input)
-    # print('pipeline: {}'.format(res))
+    if request.method == 'GET':
+      return render_template("home.html")
+    else:
+      model_checkpoint = "nguyenvulebinh/vi-mrc-large"
+      # model_checkpoint = "nguyenvulebinh/vi-mrc-base"
+      nlp = pipeline('question-answering', model=model_checkpoint,
+                        tokenizer=model_checkpoint)
+      QA_input = {
+        'question': "Bình là chuyên gia về gì ?",
+        'context': "Bình Nguyễn là một người đam mê với lĩnh vực xử lý ngôn ngữ tự nhiên . Anh nhận chứng chỉ Google Developer Expert năm 2020"
+      }
+      res = nlp(QA_input)
+      print('pipeline: {}'.format(res))
 
-    return "Huyen"
+      return "Huyen"
 
 
 if __name__ == '__main__':
