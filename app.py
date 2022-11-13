a6ca362f85f3e78a26cb3f1d7cfac7501cb48bf6
huyenxam <huyenxam250400@gmail.com>
2022-11-13 23:21:09 +0700

    first commit

--- app.py
+++ app.py
@@ -8,18 +8,11 @@ def home():
     if request.method == 'GET':
       return render_template("home.html")
     else:
-      model_checkpoint = "nguyenvulebinh/vi-mrc-large"
-      # model_checkpoint = "nguyenvulebinh/vi-mrc-base"
-      nlp = pipeline('question-answering', model=model_checkpoint,
-                        tokenizer=model_checkpoint)
-      QA_input = {
-        'question': "Bình là chuyên gia về gì ?",
-        'context': "Bình Nguyễn là một người đam mê với lĩnh vực xử lý ngôn ngữ tự nhiên . Anh nhận chứng chỉ Google Developer Expert năm 2020"
-      }
-      res = nlp(QA_input)
-      print('pipeline: {}'.format(res))
+      context =  request.form['context']
+      question =  request.form['question']
 
-      return "Huyen"
+      answer = "Hệ thống đang chờ cập nhật"
+      return render_template("home.html", context=context, question=question, answer=answer)
 
 
 if __name__ == '__main__':
