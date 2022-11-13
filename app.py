862f7c3e05f710dc3aa71b9ffafdbe0bd9bc830f
huyenxam <huyenxam250400@gmail.com>
2022-11-13 16:36:50 +0700

    first commit

--- app.py
+++ app.py
@@ -9,12 +9,12 @@ def home():
     # model_checkpoint = "nguyenvulebinh/vi-mrc-base"
     nlp = pipeline('question-answering', model=model_checkpoint,
                       tokenizer=model_checkpoint)
-    QA_input = {
-      'question': "Bình là chuyên gia về gì ?",
-      'context': "Bình Nguyễn là một người đam mê với lĩnh vực xử lý ngôn ngữ tự nhiên . Anh nhận chứng chỉ Google Developer Expert năm 2020"
-    }
-    res = nlp(QA_input)
-    print('pipeline: {}'.format(res))
+    # QA_input = {
+    #   'question': "Bình là chuyên gia về gì ?",
+    #   'context': "Bình Nguyễn là một người đam mê với lĩnh vực xử lý ngôn ngữ tự nhiên . Anh nhận chứng chỉ Google Developer Expert năm 2020"
+    # }
+    # res = nlp(QA_input)
+    # print('pipeline: {}'.format(res))
 
     return "Huyen"
 
