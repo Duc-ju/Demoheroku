083c251130d9e2794ec16a29a1b153f128fe575e
huyenxam <huyenxam250400@gmail.com>
2022-11-19 10:25:34 +0700

    first commit

--- app.py
+++ app.py
@@ -1,21 +1,28 @@
-from flask import Flask, render_template, request
-# from model import predict
+# from flask import Flask, render_template, request
+# # from model import predict
+# app = Flask(__name__)
+
+# @app.route("/demo", methods=['GET', 'POST'])
+# def home():
+#     if request.method == 'GET':
+#       return render_template("home.html")
+#     else:
+#       context =  request.form['context']
+#       question =  request.form['question']
+
+#       # answer = predict(question=question, context=context)
+#       return render_template("home.html", context=context, question=question, answer="answer")
+
+
+# if __name__ == '__main__':
+#     app.run()
+
+
+
+from flask import Flask
 app = Flask(__name__)
-
-@app.route("/demo", methods=['GET', 'POST'])
-def home():
-    if request.method == 'GET':
-      return render_template("home.html")
-    else:
-      context =  request.form['context']
-      question =  request.form['question']
-
-      # answer = predict(question=question, context=context)
-      return render_template("home.html", context=context, question=question, answer="answer")
-
-
+@app.route('/')
+def hello_world():
+    return "Hello World!"
 if __name__ == '__main__':
-    app.run()
-
-
-
+    app.run(debug=True,host='0.0.0.0')
