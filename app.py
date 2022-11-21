4eb54189f9f6a452f2a23bddf719cd08d82bd70b
huyenxam <huyenxam250400@gmail.com>
2022-11-21 21:07:58 +0700

    first commit

--- app.py
+++ app.py
@@ -25,4 +25,4 @@ app = Flask(__name__)
 def hello_world():
     return "Hello World!"
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0', post=4442)
+    app.run(debug=True,host='0.0.0.0', port=4442)
